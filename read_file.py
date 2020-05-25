33c9a5bb2fcfc0439df07f10b0f1a68eb5ef4585
Goh Pi Han <51304920+nahiphog@users.noreply.github.com>
2020-05-25 13:42:07 +0800

    Create read_file.py

---
+++ read_file.py
@@ -0,0 +1,15 @@
+
+
+states = open('all_usa_states.txt', 'r').read().splitlines()
+
+## 'r' <<<< READ
+## 'w' <<<< WRITE / OVERWRITE
+## 'a' <<<< APPEND / ADD
+
+read()
+splitlines()
+strip()
+close()
+
+from google.colab import files
+uploaded = files.upload() 
