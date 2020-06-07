79dccce5796c36512e4e0d66fd186a24e29629db
Goh Pi Han <51304920+nahiphog@users.noreply.github.com>
2020-06-07 15:18:32 +0800

    Update debugging_tools.py

--- debugging_tools.py
+++ debugging_tools.py
@@ -1,16 +1,11 @@
 x = "hello"
 
-#if condition returns True, then nothing happens:
-assert x == "hello"
-
 #if condition returns False, AssertionError is raised:
-assert x == "goodbye"
-
+assert x == "goodbye", "x should be 'hello'"
 
 '''
 Output
 Traceback (most recent call last):
   File "demo_ref_keyword_assert.py", line 5, in <module>
-    assert x == "goodbye"
-AssertionError
+AssertionError: x should be 'hello'
 '''
